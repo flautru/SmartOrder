502c9e43d85ae1315632e513df2e2b0f47cbd7a2
flautru <flautru@gmail.com>
2025-07-24 11:44:32 +0200

    Update DeliveryFactory.java

--- src/main/java/com/fabien/smart_order/delivery/factory/DeliveryFactory.java
+++ src/main/java/com/fabien/smart_order/delivery/factory/DeliveryFactory.java
@@ -1,15 +1,17 @@
 package com.fabien.smart_order.delivery.factory;
 
 import com.fabien.smart_order.delivery.DeliveryMethod;
-import com.fabien.smart_order.delivery.impl.ChronopostDelivery;
-import com.fabien.smart_order.delivery.impl.ColissimoDelivery;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class DeliveryFactory {
-    public static DeliveryMethod create(String type) {
-        return switch (type.toUpperCase()) {
-            case "COLISSIMO" -> new ColissimoDelivery();
-            case "CHRONOPOST" -> new ChronopostDelivery();
-            default -> throw new IllegalArgumentException("Methode de livraison inconnue");
-        };
+
+    private final Map<String, DeliveryMethod> deliveryMethods;
+
+    public DeliveryFactory(List<DeliveryMethod> methods) {
+        this.deliveryMethods = methods.stream()
+            .collect(Collectors.toMap(DeliveryMethod::getLabel, Function.identity()));
     }
 }
